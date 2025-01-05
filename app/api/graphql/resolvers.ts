4cdc11254d2d28474b8a9ad0c282a46b5caff4ef
Deeksha Prabhakar <deekiyengar@gmail.com>
2025-01-04 16:00:34 -0800

    Lesson 2

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -1,3 +1,49 @@
-const resolvers = {}
+const resolvers = {
+  COLORS: {
+    RED: '#f00',
+    GREEN: '#0f0',
+    BLUE: '#00f',
+  },
+  SearchType: {
+    __resolveType(obj) {
+      if (obj.species) {
+        return 'Animal'
+      }
+      if (obj.name) {
+        return 'Person'
+      }
+      return null
+    },
+  },
+  Person: {
+    name: (parent) => {
+      return parent.name.toUpperCase()
+    },
+    pets: (parent) => {
+      return parent.pets.map((id) => {
+        return { species: 'Dog', name: 'Rex' }
+      })
+    },
+  },
+  Query: {
+    search: () => {
+      return [
+        { species: 'Dog', name: 'Rex' },
+        { species: 'Cat', name: 'Fluffy' },
+        { id: 'Person', name: 'John Doe' },
+      ]
+    },
+    me: () => {
+      return 'Hello world!'
+    },
+    people: () => {
+      return [
+        { name: 'John Doe', id: 1, faveColor: 'RED', pets: [1, 2, 3] },
+        { name: 'Jane Doe', id: 2, faveColor: 'GREEN', pets: [] },
+        { name: 'John Smith', id: 3, faveColor: 'BLUE', pets: [] },
+      ]
+    },
+  },
+}
 
 export default resolvers
--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,5 +1,31 @@
 const schema = `#graphql
 
+    enum COLORS {
+        RED
+        GREEN
+        BLUE
+    }
+
+    type Animal {
+        species: String!
+        name: String!
+    }
+
+    type Person {
+        name: String!
+        id: ID!
+        faveColor: COLORS!
+        pets: [Animal]!
+    }
+
+    union SearchType = Animal | Person
+
+    type Query {
+        me: String
+        people: [Person]
+        search: [SearchType]
+    }
+    
 `
 
 export default schema
