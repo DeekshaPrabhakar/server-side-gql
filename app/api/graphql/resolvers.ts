5ee2f5deb612230a9c6f5ece30d4be32d032fb54
Deeksha Prabhakar <deekiyengar@gmail.com>
2025-01-15 15:02:48 -0800

    crud operations

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -11,6 +11,73 @@ export const resolvers = {
     me: (_, __, ctx: GQLContext) => {
       return ctx.user
     },
+    issues: async (
+      _,
+      {
+        input,
+      }: {
+        input?: {
+          statuses?: SelectIssues['status'][]
+          projects?: SelectIssues['projectId'][]
+        }
+      },
+      ctx: GQLContext
+    ) => {
+      if (!ctx.user)
+        throw new GraphQLError('UNAUTHORIZED', { extensions: { code: 401 } })
+
+      // return only issues that belong to the user
+      const andFilters = [eq(issues.userId, ctx.user.id)]
+
+      if (input && input.statuses && input.statuses.length) {
+        const statusFilters = input.statuses.map((status) =>
+          eq(issues.status, status)
+        )
+
+        andFilters.push(or(...statusFilters))
+      }
+
+      const data = await db.query.issues.findMany({
+        where: and(...andFilters),
+        orderBy: [
+          asc(sql`case ${issues.status}
+        when "backlog" then 1
+        when "inprogress" then 2
+        when "done" then 3
+      end`),
+          desc(issues.createdAt),
+        ],
+      })
+
+      return data
+    },
+  },
+  IssueStatus: {
+    BACKLOG: 'backlog', // always need to resolve enums since in graphwl the enum is uppercase and in db is lowercase
+    TODO: 'todo',
+    INPROGRESS: 'inprogress',
+    DONE: 'done',
+  },
+  Issue: {
+    // this is a resolver for the Issue type and say how to resolve user
+    user: (issue, args, ctx) => {
+      if (!ctx.user)
+        throw new GraphQLError('UNAUTHORIZED', { extensions: { code: 401 } })
+
+      return db.query.users.findFirst({
+        where: eq(users.id, issue.userId),
+      })
+    },
+  },
+  User: {
+    issues: (user, args, ctx) => {
+      if (!ctx.user)
+        throw new GraphQLError('UNAUTHORIZED', { extensions: { code: 401 } })
+
+      return db.query.issues.findMany({
+        where: eq(issues.userId, user.id),
+      })
+    },
   },
   Mutation: {
     signin: async (_, args) => {
@@ -34,6 +101,33 @@ export const resolvers = {
       }
 
       return { ...data.user, token: data.token }
+    },
+    createIssue: async (_, { input }, ctx: GQLContext) => {
+      if (!ctx.user)
+        throw new GraphQLError('UNAUTHORIZED', { extensions: { code: 401 } })
+
+      const issue = await db
+        .insert(issues)
+        .values({ ...input, userId: ctx.user.id })
+        .returning()
+      //returning is a method that returns the inserted row instead of stats on how many rows were inserted
+
+      return issue[0]
+    },
+    editIssue: async (_, { input }, ctx) => {
+      if (!ctx.user)
+        throw new GraphQLError('UNAUTHORIZED', { extensions: { code: 401 } })
+
+      console.log('input', input)
+      const { id, ...update } = input
+
+      const result = await db
+        .update(issues)
+        .set(update ?? {})
+        .where(and(eq(issues.userId, ctx.user.id), eq(issues.id, id)))
+        .returning()
+
+      return result[0]
     },
   },
 }
--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,9 +1,38 @@
 export const schema = `#graphql
+    type Issue {
+        id: ID!
+        name: String!
+        content: String!
+        createdAt: String!
+        userId: String!
+        user: User!
+        status: IssueStatus
+        projectId: String!
+    }
+
+    enum IssueStatus {
+        BACKLOG
+        TODO
+        INPROGRESS
+        DONE
+    }
+
+    input CreateIssueInput {
+        name: String!
+        content: String!
+        status: IssueStatus
+    }
+
     type User {
         id: ID!
         email: String!
         token: String!
         createdAt: String!
+        issues: [Issue]! # add issues field
+    }
+
+    input IssuesFilterInput {
+        statuses: [IssueStatus!]
     }
 
     input AuthInput {
@@ -11,13 +40,23 @@ export const schema = `#graphql
         password: String!
     }
     
+    input EditIssueInput {
+        name: String
+        content: String
+        status: IssueStatus
+        id: ID!
+    }
     
     type Query {
         me: User
+        issues(input: IssuesFilterInput): [Issue]!
     }
 
     type Mutation {
         signin(input: AuthInput!): User
         createUser(input: AuthInput!): User
+        createIssue(input: CreateIssueInput!): Issue!
+        editIssue(input: EditIssueInput!): Issue!
     }
+
 `
